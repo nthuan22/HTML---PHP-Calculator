9a935f468422043a9b4cae3e0b6fd6b65a2c4fe2
nthuan22 <53529609+nthuan22@users.noreply.github.com>
2020-10-21 12:38:39 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+<!--Your code goes here-->
+<h3>Addition</h3>
+<form method="get" action="addition.php">
+      First number: <input type="number" name="first"><br>
+      Second number: <input type="number" name="second"><br>
+      <button type="submit">Add</button><br>
+</form>
+
+<br>
+<h3>Division</h3>
+<form method="get" action="division.php">
+      Numerator: <input type="number" name="numerator"><br>
+      Denominator: <input type="number" name="denominator"><br>
+      <button type="submit">Divide</button><br>
+</form>
+
+<a href="index.php">Reset</a>
+</body>
+</html>
