c807ffc7f3ae929a7709003ddddc02885653d299
nthuan22 <53529609+nthuan22@users.noreply.github.com>
2020-10-21 12:39:07 +0700

    Create addition.php

---
+++ addition.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<!--Your code goes here-->
+<!-- <?php print_r($_GET) ?> -->
+<?="The sum of ${_GET['first']} and ${_GET['second']} is:"?>
+<h4> <?= $_GET['first'] + $_GET['second'] ?> </h4>
+
+<!--
+<h3>Addition</h3>
+<form>
+      First number: <input type="number"><br>
+      Second number: <input type="number"><br>
+      <button type="submit">Add</button><br>
+</form>
+-->
+
+<a href="index.php">Reset</a>
+</body>
+</html>
