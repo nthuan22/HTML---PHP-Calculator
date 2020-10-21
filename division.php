be6159ba2fa4b1e9d8836bcd59ab53a9a5820e8d
nthuan22 <53529609+nthuan22@users.noreply.github.com>
2020-10-21 12:39:25 +0700

    Create division.php

---
+++ division.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+<!--Your code goes here-->
+<!-- <?php print_r($_GET) ?> -->
+<?="The result of ${_GET['numerator']} devides by ${_GET['denominator']} is: "?>
+<h4> <?=$_GET['numerator'] / $_GET['denominator'] ?> </h4>
+
+<!--
+<h3>Division</h3>
+<form> 
+      Numerator: <input type="number"><br>
+      Denominator: <input type="number"><br>
+      <button type="submit">Divide</button><br>
+</form>
+-->
+
+
+<a href="index.php">Reset</a>
+</body>
+</html>
